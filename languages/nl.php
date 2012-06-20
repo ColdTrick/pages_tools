56fda28bb9363bb68514e7d249105024e7824d1f
Jerome Bakker <jeabakker@coldtrick.com>
2012-06-20 11:13:51 +0200

    added: Dutch translation

---
+++ languages/nl.php
@@ -0,0 +1,21 @@
+<?php
+$language = array (
+  'pages_tools:allow_comments' => 'Sta commentaar toe',
+  'pages_tools:notify:edit:subject' => 'Je pagina \'%s\' is bijgewerkt',
+  'pages_tools:notify:edit:message' => 'Hallo,
+
+Je pagina \'%s\' is bijgewerkt door %s. Om de nieuwe versie te zien, klik hier:
+%s',
+  'pages_tools:export:title' => 'Export instellingen',
+  'page_tools:export:format' => 'Pagina formaat',
+  'page_tools:export:format:a4' => 'A4',
+  'page_tools:export:format:letter' => 'Letter',
+  'page_tools:export:format:a3' => 'A3',
+  'page_tools:export:format:a5' => 'A5',
+  'page_tools:export:include_subpages' => 'Inclusief subpagina\'s',
+  'page_tools:export:include_index' => 'Inclusief index',
+  'pages_tools:export:index' => 'Inhoudsopgave',
+  'pages_tools:actions:reorder:error:subpages' => 'Er zijn geen pagina\'s opgegeven om te herordenen',
+  'pages_tools:actions:reorder:success' => 'De pagina\'s zijn succesvol herordenend',
+);
+add_translation("nl", $language);
