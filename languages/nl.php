f50804f572913ea3a37d8f20fd327b6c3c99c60c
Jerome Bakker <jeabakker@coldtrick.com>
2012-08-15 14:10:45 +0200

    updated: Dutch translation

--- languages/nl.php
+++ languages/nl.php
@@ -1,12 +1,35 @@
 <?php
 $language = array (
+  'pages_tools:unpublished' => 'Niet gepubliceerd',
+  'pages_tools:notify:publish:subject' => 'Een pagina is gepubliceerd',
+  'pages_tools:notify:publish:message' => 'LS,
+
+je pagina \'%s\' is gepubliceerd.
+
+Je kunt de pagina hier bekijken:
+%s',
+  'pages_tools:notify:expire:subject' => 'Een pagina is verlopen',
+  'pages_tools:notify:expire:message' => 'LS,
+
+je pagina \'%s\' is verlopen.
+
+Je kunt de pagina hier bekijken:
+%s',
+  'pages_tools:widgets:index_pages:description' => 'Toont de laatste pagina\'s op je community',
+  'pages_tools:settings:advanced_publication' => 'Schakel geavanceerde publicatie opties in',
+  'pages_tools:settings:advanced_publication:description' => 'Als dit is ingeschakeld kunnen gebruikers een publicatie- en vervaldatum voor pagina\'s opgeven. Vereist een werkende dagelijkse CRON.',
+  'pages_tools:label:publication_options' => 'Publicatie opties',
+  'pages_tools:label:publication_date' => 'Publicatie datum (optioneel)',
+  'pages_tools:publication_date:description' => 'Als je hier een datum selecteert zal de pagina niet worden gepubliceerd voor de aangegeven datum.',
+  'pages_tools:label:expiration_date' => 'Verval datum (optioneel)',
+  'pages_tools:expiration_date:description' => 'De pagina zal niet meer gepubliceerd zijn na de geselecteerde datum.',
   'pages_tools:allow_comments' => 'Sta commentaar toe',
   'pages_tools:notify:edit:subject' => 'Je pagina \'%s\' is bijgewerkt',
   'pages_tools:notify:edit:message' => 'Hallo,
 
 Je pagina \'%s\' is bijgewerkt door %s. Om de nieuwe versie te zien, klik hier:
 %s',
-'pages_tools:menu:title:export' => "Exporteer",
+  'pages_tools:menu:title:export' => 'Exporteer',
   'pages_tools:export:title' => 'Export instellingen',
   'page_tools:export:format' => 'Pagina formaat',
   'page_tools:export:format:a4' => 'A4',
